8d20192c8c4718574cd7f6849020e2288b9cee20
nikhilpounikar <nikhilpounikar@gmail.com>
2023-11-07 13:57:18 +0530

    code refactored using custom context provider and hook

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,30 +3,30 @@ import styles from "../styles/ItemCard.module.css";
 import { useValue } from "../itemContext";
 
 function ItemCard({ name, price }) {
-  const { item, setItem,total, setTotal } = useValue();
+  const { handleAdd,handleRemove} = useValue();
 
-  const handleAdd = () => {
-    setTotal(total + price);
-    setItem(item+1);
-  };
+  // const handleAdd = () => {
+  //   setTotal(total + price);
+  //   setItem(item+1);
+  // };
 
-  const handleRemove = () => {
-    if (total <= 0) {
-      return;
-    }
-    setTotal((prevState) => prevState - price);
-    setItem(item-1)
-  };
+  // const handleRemove = () => {
+  //   if (total <= 0) {
+  //     return;
+  //   }
+  //   setTotal((prevState) => prevState - price);
+  //   setItem(item-1)
+  // };
 
   return (
     <div className={styles.itemCard}>
       <div className={styles.itemName}>{name}</div>
       <div className={styles.itemPrice}>&#x20B9; {price}</div>
       <div className={styles.itemButtonsWrapper}>
-        <button className={styles.itemButton} onClick={() => handleAdd()}>
+        <button className={styles.itemButton} onClick={() => handleAdd(price)}>
           Add
         </button>
-        <button className={styles.itemButton} onClick={() => handleRemove()}>
+        <button className={styles.itemButton} onClick={() => handleRemove(price)}>
           Remove
         </button>
       </div>
--- src/itemContext.js
+++ src/itemContext.js
@@ -12,8 +12,22 @@ function CustomItemContext({ children }) {
   const [total, setTotal] = useState(0);
   const [item, setItem] = useState(0);
 
+
+  const handleAdd = (price) => {
+    setTotal(total + price);
+    setItem(item+1);
+  };
+
+  const handleRemove = (price) => {
+    if (total <= 0) {
+      return;
+    }
+    setTotal((prevState) => prevState - price);
+    setItem(item-1)
+  };
+
   return (
-    <itemContext.Provider value={{ total, setTotal, item, setItem }}>
+    <itemContext.Provider value={{ total, setTotal, item, setItem,handleRemove,handleAdd }}>
       {children}
     </itemContext.Provider>
   );
